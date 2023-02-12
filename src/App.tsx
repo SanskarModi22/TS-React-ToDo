8fffbc9e676c238da1eb302a0af24ec0e0f2ccdb
Sanskar Modi <modisanskar5@gmail.com>
2023-02-12 10:42:02 +0530

    Cross Component Communication

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,12 @@ import TodoList from './components/TodoList';
 import NewTodo from './components/NewTodo';
 const App: React.FC = () => {
   const todos = [{id: 't1', text: 'Finish the course'}]; 
+  const todoHandler = (text:string)=>{
+    console.log(text);
+  }
   return (
     <div className="App">
-      <NewTodo/>
+      <NewTodo onAddTodo={todoHandler}/>
       <TodoList items ={todos}/>
     </div>
   );

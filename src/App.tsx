6b0c21a41687543fa225583e79eb69dc988b1150
Sanskar Modi <modisanskar5@gmail.com>
2023-02-12 10:56:58 +0530

    Managing State Bettter

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,9 @@ const App: React.FC = () => {
   const [todos,setTodo]=useState<Todo[]>([]);
   // const todos = [{id: 't1', text: 'Finish the course'}]; 
   const todoHandler = (text:string)=>{
-    setTodo([{id:Math.random().toString(),text:text}]);
+    setTodo(prevTodos=>[...prevTodos,{id:Math.random().toString(),text:text}]);
+    //The argument prevTodos is an array of existing to-do items.
+    //The spread operator (...) is used to create a shallow copy of the prevTodos array. This ensures that the original array is not modified directly.
   }
   return (
     <div className="App">
